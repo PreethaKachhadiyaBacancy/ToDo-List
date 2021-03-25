5ae50435651afe55e13291db14f4a24a14dfb07d
Preetha Kachhadiya <preetha.kachhadiya@bacancy.com>
2021-03-25 12:07:17 +0530

    UI with Bootstrap vue nd changes in edit and delete functionality

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="formDiv">
+  <!-- <div class="formDiv">
     <form @submit.prevent="addTodo">
       <input
         type="text"
@@ -29,7 +29,82 @@
         No todo to Show. Please add a new Todo.
       </h2>
     </div>
-  </div>
+  </div> -->
+  <b-container class="container">
+    <b-row>
+      <b-col>
+        <b-form @submit.prevent="addTodo">
+          <b-row align-h="start">
+            <b-col cols="9" class="inputRow">
+              <b-form-input
+                v-model="newTodo"
+                type="text"
+                placeholder="Create a new to-do..."
+              ></b-form-input>
+            </b-col>
+            <b-col cols="3" class="submitBtn">
+              <b-button type="submit" variant="warning">Add</b-button>
+            </b-col>
+          </b-row>
+        </b-form>
+      </b-col>
+    </b-row>
+
+    <b-row align-h="start">
+      <b-col cols="4">
+        <b-form-radio-group
+          v-model="listVisibility"
+          buttons
+          button-variant="outline-primary"
+        >
+          <b-form-radio value="all">All</b-form-radio>
+          <b-form-radio value="completed">Completed</b-form-radio>
+          <b-form-radio value="uncompleted">Uncompleted</b-form-radio>
+        </b-form-radio-group>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col>
+        <b-row>
+          <b-list-group v-if="todoList" style="width: 100%">
+            <b-list-group-item v-for="todo in todoList" :key="todo.id">
+              <b-col cols="1" class="aa">
+                <b-form-checkbox
+                  @click="toggleCompletedStatus(todo.id)"
+                ></b-form-checkbox>
+              </b-col>
+              <b-col cols="8" class="aa">
+                <div v-if="!(editId === todo.id)">
+                  <span>{{ todo.value }}</span>
+                </div>
+                <div v-else>
+                  <b-form-input
+                    v-model="editTodo"
+                    @blur="finishEditTodo(todo.id)"
+                  ></b-form-input>
+                </div>
+              </b-col>
+              <b-col cols="3">
+                <b-button
+                  variant="outline-primary"
+                  @click="startEditTodo(todo.id)"
+                  class="editBtn"
+                  >Edit</b-button
+                >
+                <b-button
+                  variant="outline-danger"
+                  @click="deleteTodo(todo.id)"
+                  class="deleteBtn"
+                  >Delete</b-button
+                >
+              </b-col>
+            </b-list-group-item>
+          </b-list-group>
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -44,11 +119,14 @@ export default {
       editTodo: "",
       todoList: null,
       editId: null,
+      listVisibility: "all",
     };
   },
-  computed: {},
   created() {
     this.getTodo();
+  },
+  watch: {
+
   },
   methods: {
     addTodo() {
@@ -56,6 +134,7 @@ export default {
         const todo = {
           id: new Date().toISOString(),
           value: this.newTodo,
+          completed: false,
         };
 
         Service.post(`todo.json`, todo)
@@ -84,6 +163,7 @@ export default {
       Service.get(`todo.json`)
         .then((res) => {
           this.todoList = res.data;
+          console.log("todoList:", this.todoList);
         })
         .catch((error) =>
           this.$toaster.error(
@@ -117,13 +197,12 @@ export default {
 
       Service.put(`todo/${editId}.json`, editedTodo)
         .then(() => {
-          this.getTodo();
+          this.todoList[editId].value = editedTodo.value;
           this.editId = null;
-          setTimeout(() => {
-            this.$toaster.success("Todo edited Succesfully!", {
-              timeout: 3000,
-            });
-          }, 400);
+          this.$toaster.success("Todo edited Succesfully!", {
+            timeout: 3000,
+          });
+          
         })
         .catch((error) =>
           this.$toaster.error("Error in Editing Todo." + error, {
@@ -138,17 +217,34 @@ export default {
 
         Service.delete(`todo/${deleteId}.json`)
           .then(() => {
-            this.getTodo();
+            delete this.todoList[deleteId];
 
-            setTimeout(() => {
-              this.$toaster.success("Todo deleted Succesfully!", {
-                timeout: 3000,
-              });
-            }, 1500);
+            this.todoList = {
+              ...this.todoList,
+            };
+
+            console.log(this.todoList);
+
+            // const tempTodoList = this.todoList.fi
+
+            // const tempTodoList = arrayTodo.filter(([key, value]) => console.log(key, value.id));
+
+            // this.todoList = {
+            //   ...tempTodoList
+            // };
+
+            this.$toaster.success("Todo deleted Succesfully!", {
+              timeout: 3000,
+            });
+            // setTimeout(() => {
+            //   this.$toaster.success("Todo deleted Succesfully!", {
+            //     timeout: 3000,
+            //   });
+            // }, 500);
           })
           .catch((error) =>
             this.$toaster.error("Error in Deleting Todo." + error, {
-              timeout: 3000,
+              timeout: 30000,
             })
           );
       }
@@ -158,12 +254,12 @@ export default {
 </script>
 
 <style scoped>
-.formDiv {
+/* .formDiv {
   width: 70%;
   margin: 0 auto;
-}
+} */
 
-input[type="text"] {
+/* input[type="text"] {
   border-radius: 5px;
   border: 1px solid lightgray;
   height: 32px;
@@ -172,30 +268,73 @@ input[type="text"] {
   padding-left: 5px;
   font-size: 100%;
   margin: 0;
+} */
+
+.container {
+  width: 70%;
 }
 
-button[type="submit"] {
-  display: inline-block;
-  width: 10%;
-  height: 36px;
-  border-radius: 5px;
-  background-color: lightpink;
-  margin-left: 2%;
-  font-size: 110%;
-  cursor: pointer;
-}
-
-.listDiv {
-  margin: 5% auto;
-}
-
-ul {
-  list-style: none;
-  margin: 0;
+.inputRow,
+.submitBtn {
   padding: 0;
 }
 
-li {
+.submitBtn {
+  padding-left: 5%;
+}
+
+button[type="submit"] {
+  /* display: inline-block; */
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  padding: 0;
+  /* background-color: lightpink; */
+  /* margin-left: 2%; */
+  font-size: 120%;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.row {
+  margin: 2% auto;
+}
+
+/* .list-group { */
+/* display: flex; */
+/* flex-direction: row; */
+/* align-items: center; */
+/* } */
+
+.list-group-item {
+  display: flex;
+  align-items: space-between;
+  text-align: left;
+  align-content: center;
+  padding: 1%;
+  /* flex-direction: ; */
+}
+
+.aa {
+  margin: auto 0;
+}
+
+.deleteBtn,
+.editBtn {
+  margin: 0 3%;
+}
+
+/* .listDiv {
+  margin: 5% auto;
+} */
+
+/* ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+} */
+
+/* li {
   border: 1px solid lightgray;
   margin: 0 auto;
   padding: 1%;
@@ -203,30 +342,30 @@ li {
   border-radius: 5px;
   display: flex;
   flex-direction: row;
-}
+} */
 
-li > div {
+/* li > div {
   width: 70%;
   float: inline-start;
-}
+} */
 
-li > button {
+/* li > button {
   display: inline-block;
-}
+} */
 
-.edit {
+/* .edit {
   width: 10%;
   background-color: lightseagreen;
   cursor: pointer;
   float: inline-start;
   height: 25px;
   margin-right: 2%;
-}
+} */
 
-.delete {
+/* .delete {
   display: inline-block;
   width: 10%;
   background-color: rgb(247, 54, 92);
   cursor: pointer;
-}
+}  */
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Toaster from 'v-toaster'
 import 'v-toaster/dist/v-toaster.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.use(Toaster);
 
